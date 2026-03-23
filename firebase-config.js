366991f92e76a7288e9294b80993a6fcda3816fc
Neseli27 <neseli27@gmail.com>
2026-03-23 08:28:22 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,13 @@
 // Kendi Firebase proje bilgilerinizi buraya girin
 
 const firebaseConfig = {
-    apiKey: "BURAYA_API_KEY",
-    authDomain: "BURAYA_AUTH_DOMAIN",
-    projectId: "BURAYA_PROJECT_ID",
-    storageBucket: "BURAYA_STORAGE_BUCKET",
-    messagingSenderId: "BURAYA_SENDER_ID",
-    appId: "BURAYA_APP_ID"
+    apiKey: "AIzaSyCqUSoowo2EbKKhG0SBcIzBYddwYOzHKRo",
+  authDomain: "egitim-yonetim-platformu.firebaseapp.com",
+  projectId: "egitim-yonetim-platformu",
+  storageBucket: "egitim-yonetim-platformu.firebasestorage.app",
+  messagingSenderId: "548967060709",
+  appId: "1:548967060709:web:6e0e360c8010824d34700c",
+  measurementId: "G-MNR3YS5Z5J"
 };
 
 firebase.initializeApp(firebaseConfig);
